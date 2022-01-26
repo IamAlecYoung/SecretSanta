5ed8b81a6dc478ab6c3de347801684eba6ca27ae
IamAlecYoung <IamAlecYoung@users.noreply.github.com>
2022-01-26 21:20:18 +0000

    Update Startup.cs

--- SecretSanta.Web/Startup.cs
+++ SecretSanta.Web/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Hosting;
 using SecretSanta.Web.Data;
 using Microsoft.EntityFrameworkCore;
